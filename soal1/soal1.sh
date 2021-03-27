7d9e5c21b7322554caf223d429a9d9021d3d4187
Christoffer Ivano <73484021+christofferivano@users.noreply.github.com>
2021-03-27 19:41:27 +0700

    fix bc

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-#a.
-grep -o "[I|E].*" syslog.log
+#a. cari error dan info
+grep -o "[E|I].*" syslog.log
 
 #b. cari error dan jumlahnya
+grep -o "ERROR.*" syslog.log | cut -d "(" -f1 | sort | uniq -c
+
+#c. cari error dan info
+grep -o "ERROR.*" syslog.log | cut -d "(" -f2 | cut -d ")" -f1 | uniq -c
+grep -o "INFO.*" syslog.log | cut -d "(" -f2 | cut -d ")" -f1 | uniq -c
+
+#d. cari error dan jumlahnya masukkan ke csv
 modified=$(grep "The ticket was modified while updating" syslog.log | wc -l)
 cTicket=$(grep "Permission denied while closing ticket" syslog.log | wc -l)
 closed=$(grep "Tried to add information to closed ticket" syslog.log | wc -l)
 timeout=$(grep "Timeout while retrieving information" syslog.log | wc -l)
 noExist=$(grep "Ticket doesn't exist" syslog.log | wc -l)
 connection=$(grep "Connection to DB failed" syslog.log | wc -l)
-
-#c.d. print error dan info
-echo "Error,Count" > error_info.csv
+echo "Error,Count" > error_message.csv
 printf "The ticket was modified while updating,%d\n
 Permission denied while closing ticket,%d\n
 Tried to add information to closed ticket,%d\n
 Timeout while retrieving information,%d\n
 Ticket doesn't exist,%d\n
 Connection to DB failed,%d\n" $modified $cTicket $closed $timeout $noExist $connection|
-sort -t, -nr -k2 >> error_info.csv
+sort -t, -nr -k2 >> error_message.csv
 
-#e.
+#e. cari username, error, info, jumlah
 tr ' ' '\n' < syslog.log > test.txt
 grep -o "(.*)" test.txt | tr -d "(" | tr -d ")" | sort | uniq > user_statistic.txt
 echo "Username,INFO,ERROR" > user_statistic.csv
