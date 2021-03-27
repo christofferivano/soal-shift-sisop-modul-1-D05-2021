e0886140ce489bdc83d2893c4b721862eed86d6e
amanullahddd <amanullah0daffa@gmail.com>
2021-03-27 20:25:18 +0700

    soal 3 modul 1

---
+++ soal3/soal3b.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mkdir "/home/amanullahd/$(date +"%d-%m-%Y")"
+cd "/home/amanullahd/$(date +"%d-%m-%Y")"
+for ((num=1; num<=23; num=num+1))
+do
+if [ $num -le 9 ]
+then
+wget -a Foto.log https://loremflickr.com/320/240/kitten -O "Koleksi_0$num"
+
+else
+wget -a Foto.log https://loremflickr.com/320/240/kitten -O "Koleksi_$num"
+fi
+done
+
+md5sum * | \
+sort | \
+awk 'BEGIN{lasthash = ""} $1 == lasthash {print $2} {lasthash = $1}' | \
+xargs rm
+
+i=1
+for fi in Koleksi_*; do
+        mv "$fi" Koleksi_$i
+        i=$((i+1))
+done
